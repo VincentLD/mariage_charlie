de230e21fc128afea24b93caa29f95f34fe9ed32
Vincent <vledoledec@gmail.com>
2021-11-12 18:46:42 +0100

    Responsive part

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date("December 17, 2021 00:00:00").getTime();
+    const countDate = new Date("December 17, 2021 15:00:00").getTime();
     const now = new Date().getTime();
     const gap = countDate - now;
 
